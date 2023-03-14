7beb046bd82da33c8ab90f094a7e76749afd47f0
Krisjanis Bergmanis <duejone0@gmail.com>
2023-03-14 22:31:36 +0200

    Add task for sorting array to heap

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,55 @@
 # python3
-
+import math
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+    for i in range(len(data), math.floor(len(data) / 2), -1):
+
+        root_reached = 0
+        target_index = i - 1
+        parent_index = math.ceil(target_index / 2 - 1)
+
+        while root_reached != 1:
+            target = data[target_index]
+            parent = data[parent_index]
+
+            if parent_index == 0:
+                root_reached = 1
+
+            if parent > target:
+                swaps.append([parent_index, target_index])
+                data[target_index], data[parent_index] = parent, target
+                target_index = parent_index
+                parent_index = math.ceil(target_index / 2 - 1)
+            else:
+                break
+
+    # try to achieve  O(n) and not O(n2)
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
+    # add another input for I or F
     # first two tests are from keyboard, third test is from a file
+    text = input()
+    text = "F"
+    if "F" in text:
+        file_name = input()
 
+        file = open("./tests/" + file_name, "r")
+        # file = open("./tests/" + "01", "r") # Test line
+        text = file.read()
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        text = text.split('\n')
+        n = int(text[0])
+        data = text[1].split(' ')
+
+    elif "I" in text:
+        # # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -28,9 +58,7 @@ def main():
     # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
 
     # output all swaps
     print(len(swaps))
